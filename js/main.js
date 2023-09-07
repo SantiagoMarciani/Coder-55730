78966d6bdb6160507281caccf83531f2299b392a
Santiago Marciani <santiago@pizzuti.com.ar>
2023-09-07 20:21:36 -0300

    Tercer entrega

--- js/main.js
+++ js/main.js
@@ -1,77 +1,116 @@
-
-// tabla de variables iniciales 
-
-let ahora3 = 1.10;
-let ahora6 = 1.13;
-let ahora12 = 1.18;
-let montoVenta = 0;
-
-let nombre = "";
-let producto = "";
+let carrito = [];
 let cuotas = 0;
 
-const productos = [{codigo: 1, descripcion: 'producto #1', precio: 2550}, {codigo: 2, descripcion: 'producto #2', precio: 4590}, {codigo: 3, descripcion: 'producto #3', precio: 565}]
-const planes = [{codigo: 1, plan: 'Ahora 3', coeficiente: 1.10}, {codigo: 2, plan: 'Ahora 6', coeficiente: 1.13}, {codigo: 3, plan: 'Ahora 12', coeficiente: 1.18}];
+const totalPagarElement = document.getElementById('totalPagar');
+const totalPagarCuotas = document.getElementById('totalPagarCuotas');
 
-/*Mensaje de Bienvenida*/
+// tabla de variables iniciales 
+const planes = [
+    {codigo: 0, plan: 'Sin cuotas', coeficiente: 1, cuotas: 1}, 
+    {codigo: 1, plan: 'Ahora 3', coeficiente: 1.10, cuotas: 3}, 
+    {codigo: 2, plan: 'Ahora 6', coeficiente: 1.13, cuotas: 6}, 
+    {codigo: 3, plan: 'Ahora 12', coeficiente: 1.18, cuotas: 12}];
 
-console.log(planes[0].id);
+CargarProductos();
+MostrarPromociones();
 
-function SimularCuotas() {
-    let valorcuota = 0; 
-    let totalventa = 0;
+function CargarProductos() {
+    const contenedor = document.getElementById('listaProductos');
 
-    let montoVenta = buscarEnMatriz(productos, producto);
-    let coeficiente = buscarEnMatriz(planes, cuotas);
-
-    totalventa = productos[montoVenta].precio*planes[coeficiente].coeficiente;
-    console.log(totalventa);
-    valorcuota = totalventa/3;   
-    
-    alert(nombre + " aca esta la simulación por siguiente producto: \n\nProducto: " + productos[montoVenta].codigo + "\nDescripción: " + productos[montoVenta].descripcion + "\n" + "Precio del producto $ " + productos[montoVenta].precio + "\n\n" + "El valor de cada cuota es de $ "+valorcuota.toFixed(2)+" \npagando en total $ "+totalventa.toFixed(2));
-}
-
-
-function PedirPlanFinanciacion() {
-    let plancuotas = "";
-    for(let item=0; item<planes.length; item++ ) {
-        plancuotas += planes[item].id + ". " + planes[item].plan + "\n";
-    }
-
-    cuotas = prompt("Cuantas cuotas necesita: \n\n"+plancuotas);
-
-    if (cuotas.length>0) { 
-        SimularCuotas();
-    }
-}
-
-
-function PedirIdentidad() {
-    nombre = prompt("Ingrese su nombre");
-
-    if (nombre.length > 3) {
-        PedirDatos();
-    } else {
-        let continuar = confirm("Su nombre no puede ser tan corto, \ndesea reintentar ?");
-        if (continuar) {
-            PedirIdentidad();
-        }
+    $.ajax({
+        url:'data.json',
+        type: 'POST',
+        data: {},
         
-    }
+        success: function(data){
+            for(let i=0; i<data.length; i++) {
+                console.log(data[i].nombre);
+
+                const listItem = document.createElement('li');
+                listItem.className = 'list-group-item';
+
+                const addButton = document.createElement('button');
+                addButton.className = 'btn btn-primary btn-sm float-right';
+                addButton.innerText = ' Agregar al carrito ';
+
+                listItem.innerText = data[i].nombre + '\n Precio por unidad: $ ' + data[i].precio.toFixed(2);
+                listItem.appendChild(addButton);
+
+                addButton.addEventListener('click', function() {
+                    carrito.push(data[i]);
+                    actualizarCarrito();
+                });
+                contenedor.appendChild(listItem);
+            }
+        }
+    });
 }
 
-
-function PedirDatos() {
-    let catalogo = ""
-    for(let item=0; item<productos.length; item++ ) {
-        catalogo += productos[item].codigo + ". " + productos[item].descripcion + "\n";
-    }
-
-    producto = prompt("Que producto desea simular: \n\n"+catalogo);
+function MostrarPromociones() {
+    let container   = $('#divpromociones-op');
+    let plancuotas = "";
     
-    PedirPlanFinanciacion();
+    console.log(planes);
+
+    const contenedor = document.getElementById('divpromociones');
+
+    const select = document.createElement('select');
+    select.id = "planfinanciacion";
+    select.className = 'form-control form-control-sm';
+
+    for(let item=0; item<planes.length; item++ ) {
+        const option = document.createElement('option');
+        option.text = planes[item].plan;
+        option.value = planes[item].codigo;
+        option.onclick = actualizarCarrito;
+
+        select.add(option);
+
+        plancuotas += planes[item].codigo + ". " + planes[item].plan + " Coeficiente interés: " + planes[item].coeficiente + "<br>";
+    }
+
+    container.html('PLanes de financiación: <br><br>'+plancuotas);
+    contenedor.appendChild(select);
 }
 
+function actualizarCarrito() {
+    carritoLista.innerHTML = '';
+
+    let totalPagar = 0;
+
+    cuotas = $('#planfinanciacion').val();
+
+    carrito.forEach(function(producto, index) {
+        totalPagar += producto.precio;
+
+        const carritoItem = document.createElement('li');
+        carritoItem.className = 'list-group-item';
+        carritoItem.innerText = producto.nombre + '\n Precio: ' + producto.precio.toFixed(2);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.className = 'btn btn-danger float-right ml-2';
+        deleteButton.innerText = 'Quitar';
+
+        deleteButton.addEventListener('click', function() {
+            carrito.splice(index, 1);
+            actualizarCarrito();
+        });
+
+        carritoItem.appendChild(deleteButton);
+
+        carritoLista.appendChild(carritoItem);
+    });
+
+    totalPagarElement.innerText = totalPagar.toFixed(2);
+    totalPagarCuotas.innerText = SimularCuotas(cuotas, totalPagar);
+
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+
+    const mensajeCarrito = $('#divcarrito');
+    const cantidad = carrito.length;
+
+    mensajeCarrito.html('Ver carrito (' + cantidad + ')');
+}
 
 function buscarEnMatriz(matriz, valor) {
     for(let item=0; item<matriz.length; item++ ) {
@@ -81,7 +120,52 @@ function buscarEnMatriz(matriz, valor) {
     }
 }
 
+function SimularCuotas(plan, total) {
+    let totalventa = 0;
+    let cuotasde = 0;
+    let coeficiente = buscarEnMatriz(planes, plan);
+    
+    totalventa = total*planes[coeficiente].coeficiente;
+    cuotasde = (totalventa/planes[cuotas].cuotas).toFixed(2);
+    return totalventa.toFixed(2) + ' (' + planes[cuotas].cuotas + ' cuota/s de $ ' + cuotasde + ')';
+}
 
-alert("Calculadora de importe de cuotas con tarjeta de credito \nSegunda entrega Coderhouse" );
-PedirIdentidad();
+function IniciarCompra() {
+    Swal.fire({
+        title: 'Bienvenido',
+        html: '<p>Ingrese su nombre</p> <input id="nombreIngresado" class="swal2-input" style="text-align: center; border: 1px solid black; font-size: 12pt">',                    
+        showCancelButton: true,
+        
+        cancelButtonText: 'Cancelar',
+        confirmButtonText: 'Aceptar',
+        allowOutsideClick: false,
+        background: '#fff',
+        backdrop: 'rgba(0,0,0,0.6)',
+ 
+        onOpen: function () {
+            $('#nombreIngresado').focus()
+        } 
+    }).then((result) => {
+        const nombre = document.getElementById('nombre');
+        nombre.innerText = $('#nombreIngresado').val();
+    });
+}
 
+function FinalizarCompra() {
+    swal.fire({
+        html: "<small>Muchas gracias por su compra</small><hr>",
+        type:'success',
+        icon: 'success',
+        title: 'Finalizando compra',
+        showConfirmButton: false,
+        timer: 3500    
+    })
+}
+
+
+
+const carritoGuardado = localStorage.getItem('carrito');
+if (carritoGuardado) {
+    carrito = JSON.parse(carritoGuardado);
+    actualizarCarrito();
+}
